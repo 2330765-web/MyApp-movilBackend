11893da36e6ccac85073322efb9fbba435e78cb4
Yesenia Roldan Medina <2330765@upt.edu.mx>
2026-06-10 08:54:42 -0600

    Corrige ApplicationKt module

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,8 +1,20 @@
 package com
 
-import io.ktor.server.application.Application
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
-fun Application.rootModule() {
-    configureSerialization()
-    configureRouting()
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+
+    routing {
+
+        get("/") {
+            call.respondText("Servidor Ktor funcionando en Railway")
+        }
+
+    }
 }
