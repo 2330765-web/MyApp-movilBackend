474b7db1d129dfa0710b9e9eb076f09383687457
Yesenia Roldan Medina <2330765@upt.edu.mx>
2026-06-10 09:17:15 -0600

    Corrige import Application

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -4,9 +4,7 @@ import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
-fun main(args: Array<String>) {
-    io.ktor.server.netty.EngineMain.main(args)
-}
+
 
 fun Application.module() {
 
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,13 +1,15 @@
 package com
-
-import io.ktor.server.engine.*
 import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 
-fun main(args: Array<String>) {
+fun main() {
+
     embeddedServer(
-        factory = io.ktor.server.netty.Netty,
-        port = 8080,
+        Netty,
+        port = System.getenv("PORT")?.toInt() ?: 8080,
         host = "0.0.0.0",
-        module = Application::rootModule
+        module = Application::module
     ).start(wait = true)
+
 }
